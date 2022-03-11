c52f2148fd63e6f591fef80ee9e79656d5162130
Orcicorn <orcicorn@gmail.com>
2022-03-11 11:47:33 -0500

    Tweak mustang rear

--- Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
@@ -7,3 +7,7 @@ TweakItem("Base.LightBulb","ConditionMax",2100000000);
 -- MOD: PITSTOP 2597946327
 TweakItem("Base.KleanBackPack", "WeightReduction", 95);
 TweakItem("Base.KleanBackPack", "Icon", "SBKleanPack");
+
+-- MOD: FJORD MUSTARD 2681635926
+TweakVehicle("Base.ATAMustangClassic", "rearEndHealth", 500);
+TweakVehicle("Base.ATAMustangPolice", "rearEndHealth", 500);
