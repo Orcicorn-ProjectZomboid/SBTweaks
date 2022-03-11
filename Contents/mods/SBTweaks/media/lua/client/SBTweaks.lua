152b60882364204d8b5d21a928b002f5fbba39fe
Orcicorn <orcicorn@gmail.com>
2022-03-11 11:46:30 -0500

    Initial Commit (Kleanup BackPack)

---
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
@@ -0,0 +1,9 @@
+require("ItemTweaker_Core");
+require("VehicleTweaker_Core");
+
+-- BASE GAME --------------------------
+TweakItem("Base.LightBulb","ConditionMax",2100000000);
+
+-- MOD: PITSTOP 2597946327
+TweakItem("Base.KleanBackPack", "WeightReduction", 95);
+TweakItem("Base.KleanBackPack", "Icon", "SBKleanPack");
