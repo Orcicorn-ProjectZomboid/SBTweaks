be8366c765752f04c9104929c74eeb083f7d406b
Orcicorn <orcicorn@gmail.com>
2022-03-11 11:47:46 -0500

    Tweak Jeep Rear

--- Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
@@ -11,3 +11,8 @@ TweakItem("Base.KleanBackPack", "Icon", "SBKleanPack");
 -- MOD: FJORD MUSTARD 2681635926
 TweakVehicle("Base.ATAMustangClassic", "rearEndHealth", 500);
 TweakVehicle("Base.ATAMustangPolice", "rearEndHealth", 500);
+
+-- MOD: JAAP WRUNGLE 2636100523
+TweakVehicle("Base.JeepArcher", "rearEndHealth", 500);
+TweakVehicle("Base.JeepClassic", "rearEndHealth", 500);
+TweakVehicle("Base.JeepRubicon", "rearEndHealth", 500);
