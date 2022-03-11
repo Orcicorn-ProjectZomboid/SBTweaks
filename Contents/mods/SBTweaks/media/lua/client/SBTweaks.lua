6f41269418a4e6fc962f3fe52448bc541f93d2ce
Orcicorn <orcicorn@gmail.com>
2022-03-11 11:48:41 -0500

    Improve hummer towing ability

--- Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
@@ -24,3 +24,6 @@ TweakVehicle("Base.DodgePpg", "rearEndHealth", 500);
 -- MOD: OSHGOSH 2618213077
 TweakVehicle("Base.82oshkoshM911", "frontEndHealth", 10000);
 TweakVehicle("Base.82oshkoshM911B", "frontEndHealth", 10000);
+
+-- MOD: HUMMER 2642541073
+TweakVehicle("Base.92amgeneralM998", "engineForce", 6200);
