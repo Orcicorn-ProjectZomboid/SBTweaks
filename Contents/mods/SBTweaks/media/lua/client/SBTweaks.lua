165eb12baa513744a0401bd0fb95bbe51908a0e8
Orcicorn <orcicorn@gmail.com>
2022-03-11 11:48:25 -0500

    Triple Oshgosh hood health

--- Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks.lua
@@ -20,3 +20,7 @@ TweakVehicle("Base.JeepRubicon", "rearEndHealth", 500);
 -- MOD: DADGE 2743496289
 TweakVehicle("Base.Dodge", "rearEndHealth", 500);
 TweakVehicle("Base.DodgePpg", "rearEndHealth", 500);
+
+-- MOD: OSHGOSH 2618213077
+TweakVehicle("Base.82oshkoshM911", "frontEndHealth", 10000);
+TweakVehicle("Base.82oshkoshM911B", "frontEndHealth", 10000);
