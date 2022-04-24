4675cc94f27da5c07237edc8b39fb72eafcd73ef
Orcicorn <orcicorn@gmail.com>
2022-04-24 10:48:28 -0400

    Improved Fantasy PawLowLoot spawns

--- Contents/mods/SBTweaks/media/lua/server/Distribution_Fixes.lua
+++ Contents/mods/SBTweaks/media/lua/server/Distribution_Fixes.lua
@@ -41,3 +41,44 @@ table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, "SWeapons.
 table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, 0.0025);
 table.insert(SuburbsDistributions["all"]["bin"].items, "SWeapons.WeaponMag6");
 table.insert(SuburbsDistributions["all"]["bin"].items, 0.1);
+
+/* Paw Low Loot Fantasy: 2279084780 */
+/* Grimoire */
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, "Base.grimoireclose");
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 0.005);
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "Base.grimoireclose");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.0025);
+table.insert(SuburbsDistributions["all"]["bin"].items, "Base.grimoireclosed");
+table.insert(SuburbsDistributions["all"]["bin"].items, 0.05);
+table.insert(SuburbsDistributions["all"]["postbox"].items, "Base.grimoireclose");
+table.insert(SuburbsDistributions["all"]["postbox"].items, 0.0025);
+
+/* Clothing */
+table.insert(ProceduralDistributions["list"]["ClothingStoresBoots"].items, "Base.Shoes_TKnight");
+table.insert(ProceduralDistributions["list"]["ClothingStoresBoots"].items, 0.03);
+table.insert(ProceduralDistributions["list"]["ClothingStoresDress"].items, "Base.WitchyDress");
+table.insert(ProceduralDistributions["list"]["ClothingStoresDress"].items, 0.02);
+table.insert(ProceduralDistributions["list"]["ClothingStoresGloves"].items, "Base.Gloves_TKnight");
+table.insert(ProceduralDistributions["list"]["ClothingStoresDress"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["ClothingStoresGloves"].items, "Base.KnightArms");
+table.insert(ProceduralDistributions["list"]["ClothingStoresDress"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["ClothingStoresGloves"].items, "Base.KnightArms");
+table.insert(ProceduralDistributions["list"]["ClothingStoresDress"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["ClothingStoresGlovesLeather"].items, "Base.KnightGreaves");
+table.insert(ProceduralDistributions["list"]["ClothingStoresGlovesLeather"].items, 0.03);
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, "Base.Hat_WitchyHat");
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, 0.2);
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, "Base.Hat_SpiffoEars");
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, 0.8);
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, "Base.ClericMask");
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, "Base.Hat_Tknight");
+table.insert(ProceduralDistributions["list"]["ClothingStoresHeadwear"].items, 0.2);
+table.insert(ProceduralDistributions["list"]["ClothingStoresJacketsLeather"].items, "Base.ChainMail");
+table.insert(ProceduralDistributions["list"]["ClothingStoresJacketsLeather"].items, 0.2);
+table.insert(ProceduralDistributions["list"]["ClothingStoresJacketsLeather"].items, "Base.Vest_Tknight");
+table.insert(ProceduralDistributions["list"]["ClothingStoresJacketsLeather"].items, 0.2);
+table.insert(ProceduralDistributions["list"]["ClothingStoresSocks"].items, "Base.WitchySocks");
+table.insert(ProceduralDistributions["list"]["ClothingStoresSocks"].items, 1);
+table.insert(ProceduralDistributions["list"]["ClothingStoresPants"].items, "Base.KnightWaist");
+table.insert(ProceduralDistributions["list"]["ClothingStoresPants"].items, 0.2);
