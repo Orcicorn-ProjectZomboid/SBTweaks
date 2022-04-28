1e2c8b9f04b6961a678ae05f2c964e052ba9bf2f
Orcicorn <orcicorn@gmail.com>
2022-04-28 13:50:59 -0400

    Added Rare Journals

---
+++ Contents/mods/SBTweaks/media/lua/server/SBTweaks_Distribution_Journals.lua
@@ -0,0 +1,25 @@
+require 'Items/ProceduralDistributions'
+
+-- Tyler
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.TylersPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.TylersPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.TylersPrivateJournal");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
+
+-- Ryan
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.RyansPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.RyansPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.RyansPrivateJournal");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
+
+-- Aaron
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.AaronsPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.AaronsPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.AaronsPrivateJournal");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
