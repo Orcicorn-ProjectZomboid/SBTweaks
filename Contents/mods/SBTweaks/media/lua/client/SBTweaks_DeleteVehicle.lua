9a82268777f5931d3bcea66d332a9abcd93f3ebc
Orcicorn <orcicorn@gmail.com>
2022-04-29 14:06:11 -0400

    Change to require adminwand being equipped

--- Contents/mods/SBTweaks/media/lua/client/SBTweaks_DeleteVehicle.lua
+++ Contents/mods/SBTweaks/media/lua/client/SBTweaks_DeleteVehicle.lua
@@ -3,15 +3,15 @@ local oldISVehicleMenu = ISVehicleMenu.FillMenuOutsideVehicle
 local deleteVehicle = function(player, vehicle)
     vehicle:permanentlyRemove()
     sendClientCommand(player, "vehicle", "remove", { vehicle = vehicle:getId() })
-    print("Deleted")
 end
 
 function ISVehicleMenu.FillMenuOutsideVehicle(player, context, vehicle, test)
 	oldISVehicleMenu(player, context, vehicle, test)
     local playerObj = getSpecificPlayer(player)
-    local playerInv = playerObj:getInventory()
-    local wand = playerInv:getFirstTypeRecurse("SBTweaks.AdminWand")
-    if vehicle and wand then
-        context:addOption(getText("ContextMenu_SBTweaks_DeleteVehicle"), playerObj, deleteVehicle, vehicle)
+    local equipped = playerObj:getPrimaryHandItem()
+    if vehicle and equipped then
+        if equipped:getType() == "AdminWand" then
+            context:addOption(getText("ContextMenu_SBTweaks_DeleteVehicle"), playerObj, deleteVehicle, vehicle)
+        end
     end
 end
