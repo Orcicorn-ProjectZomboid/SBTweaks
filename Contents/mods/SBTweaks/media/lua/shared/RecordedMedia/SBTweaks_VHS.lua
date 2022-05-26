3dedd4b172e002eb55e45c41d6ae3fd48bc1db39
Orcicorn <orcicorn@gmail.com>
2022-05-26 14:49:57 -0400

    VHS Additions

---
+++ Contents/mods/SBTweaks/media/lua/shared/RecordedMedia/SBTweaks_VHS.lua
@@ -0,0 +1,117 @@
+-- -------------------------------------------------------
+-- COLORS ------------------------------------------------
+-- -------------------------------------------------------
+-- Narrator     r = 1.00, g = 1.00, b = 1.00,
+-- Woodstock    r = 0.30, g = 0.70, b = 0.00,
+-- Redwing      r = 1.00, g = 0.10, b = 0.10,
+-- Orc          r = 0.70, g = 0.00, b = 0.90,
+-- Emote        r = 0.50, g = 0.50, b = 0.50,
+-- Credits      r = 0.30, g = 0.40, b = 1.00,
+-- -------------------------------------------------------
+
+-- Media: VHS: The Park is Mine[id: SBTWEAKS-RVHS-0001-0000-000000000000]
+RecMedia["SBTWEAKS-RVHS-0001-TAPE-000000000000"] = {
+	itemDisplayName = "RM_SBTWEAKS-RVHS-0001-META-000000000001",
+	title = "RM_SBTWEAKS-RVHS-0001-META-000000000002",
+	subtitle = nil,
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000001", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000002", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000003", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000004", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000005", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000006", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000007", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000008", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000009", r = 0.00, g = 0.69, b = 0.31, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000010", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0001-LINE-000000000011", r = 0.84, g = 0.50, b = 0.50, codes = "AIM+1,REL+3" },
+	},
+};
+
+-- Media: VHS: Space Balls
+RecMedia["SBTWEAKS-RVHS-0002-TAPE-000000000000"] = {
+	itemDisplayName = "RM_SBTWEAKS-RVHS-0002-META-000000000001",
+	title = "RM_SBTWEAKS-RVHS-0002-META-000000000002",
+	subtitle = nil,
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000001", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000002", r = 1.00, g = 0.65, b = 0.30, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000003", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000004", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000005", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000006", r = 1.00, g = 0.65, b = 0.30, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000007", r = 1.00, g = 0.65, b = 0.30, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000008", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000009", r = 0.50, g = 0.50, b = 0.50, codes = "ELC+1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000010", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000011", r = 0.50, g = 0.50, b = 0.50, codes = "ELC+1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000012", r = 0.30, g = 0.65, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000013", r = 0.50, g = 0.50, b = 0.50, codes = "ELC+1" },
+        { text = "RM_SBTWEAKS-RVHS-0002-LINE-000000000014", r = 1.00, g = 0.65, b = 0.30, codes = "BOR-1" },
+	},
+};
+
+-- Media: VHS: Commercial Tailoring 101
+RecMedia["SBTWEAKS-RHVS-C001-TAPE-000000000000"] = {
+	itemDisplayName = "RM_SBTWEAKS-RHVS-C001-META-000000000001",
+	title = "RM_SBTWEAKS-RHVS-C001-META-000000000002",
+	subtitle = "RM_SBTWEAKS-RHVS-C001-META-000000000003",
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000001", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000002", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000003", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000004", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000005", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000006", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000007", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000008", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000009", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000010", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000011", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000012", r = 0.00, g = 0.69, b = 0.94, codes = "TAI+1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000013", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000014", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RHVS-C001-LINE-000000000015", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+	},
+};
+
+-- Media: VHS: Space Brothers S3E3
+RecMedia["SBTWEAKS-RHVS-0003-TAPE-000000000000"] = {
+	itemDisplayName = "RM_SBTWEAKS-RHVS-SB01-META-000000000001",
+	title = "RM_SBTWEAKS-RHVS-SB01-META-000000000002",
+	subtitle = "RM_SBTWEAKS-RVHS-SB01-META-000000000003",
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000001", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Narrator
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000002", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Narrator
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000003", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000004", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000005", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },    -- Redwing
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000006", r = 1.00, g = 0.10, b = 0.10, codes = "ELC+1" },    -- Redwing
+        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000007", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000008", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },    -- Redwing
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000009", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000010", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Both Wood & Redwing
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000011", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Both Wood & Redwing
+        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000012", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },    -- Emote
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000001", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000002", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000003", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
+	},
+};
