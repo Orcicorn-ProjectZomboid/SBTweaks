5ba9834a6c18334d83d5f519350bf08f8745e1cf
Orcicorn <orcicorn@gmail.com>
2022-06-26 10:01:43 -0400

    Added Dave & Steve Journals

--- Contents/mods/SBTweaks/media/lua/server/SBTweaks_Distribution_Journals.lua
+++ Contents/mods/SBTweaks/media/lua/server/SBTweaks_Distribution_Journals.lua
@@ -2,13 +2,13 @@ require 'Items/ProceduralDistributions'
 
 -- Tyler
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.TylersPrivateJournal");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.TylersPrivateJournal");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.TylersPrivateJournal");
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.TylersPrivateJournal2");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.TylersPrivateJournal2");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.TylersPrivateJournal2");
@@ -16,13 +16,13 @@ table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
 
 -- Ryan
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.RyansPrivateJournal");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.RyansPrivateJournal");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.RyansPrivateJournal");
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.RyansPrivateJournal2");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.RyansPrivateJournal2");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.RyansPrivateJournal2");
@@ -30,14 +30,34 @@ table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
 
 -- Aaron
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.AaronsPrivateJournal");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.AaronsPrivateJournal");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.AaronsPrivateJournal");
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.AaronsPrivateJournal2");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.01);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.AaronsPrivateJournal2");
 table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.AaronsPrivateJournal2");
 table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
+
+-- Dave
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.DavesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.DavesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.DavesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "SBTweaks.DavesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, 0.001);
+
+-- Steve
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "SBTweaks.StevesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, "SBTweaks.StevesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["BinGeneric"].items, 0.001);
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "SBTweaks.StevesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 1);
+table.insert(ProceduralDistributions["list"]["ArmyStorageOutfit"].items, "SBTweaks.StevesPrivateJournal");
+table.insert(ProceduralDistributions["list"]["ArmyStorageOutfit"].items, 0.001);
