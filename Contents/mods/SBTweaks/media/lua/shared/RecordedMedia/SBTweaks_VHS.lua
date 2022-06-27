5a5a1ab6eb54f89bc4363468fef67260c3609338
Orcicorn <orcicorn@gmail.com>
2022-06-27 12:14:58 -0400

    Added Super Dave VHS & Merch Ordering

--- Contents/mods/SBTweaks/media/lua/shared/RecordedMedia/SBTweaks_VHS.lua
+++ Contents/mods/SBTweaks/media/lua/shared/RecordedMedia/SBTweaks_VHS.lua
@@ -98,20 +98,48 @@ RecMedia["SBTWEAKS-RVHS-0003-TAPE-000000000000"] = {
 	spawning = 0,
 	category = "Retail-VHS",
 	lines = {
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000001", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Narrator
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000002", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Narrator
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000003", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000004", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000005", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },    -- Redwing
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000006", r = 1.00, g = 0.10, b = 0.10, codes = "ELC+1" },    -- Redwing
-        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000007", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000008", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },    -- Redwing
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000009", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },    -- Wood
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000010", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Both Wood & Redwing
-		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000011", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },    -- Both Wood & Redwing
-        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000012", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },    -- Emote
-		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000001", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
-		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000002", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
-		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000003", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },   -- Credits
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000001", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000002", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000003", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000004", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000005", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000006", r = 1.00, g = 0.10, b = 0.10, codes = "ELC+1" },
+        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000007", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000008", r = 1.00, g = 0.10, b = 0.10, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000009", r = 0.30, g = 0.70, b = 0.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000010", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000011", r = 0.40, g = 1.00, b = 1.00, codes = "BOR-1" },
+        { text = "RM_SBTWEAKS-RVHS-SB01-LINE-000000000012", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000001", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000002", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },
+		{ text = "RM_SBTWEAKS-RVHS-SBSH-CRED-000000000003", r = 0.30, g = 0.40, b = 1.00, codes = "BOR-10" },
+	},
+};
+
+-- Media: VHS: Super Dave Stunts #1
+RecMedia["SBTWEAKS-RVHS-0004-TAPE-000000000000"] = {
+	itemDisplayName = "RM_SBTWEAKS-RVHS-SD01-META-000000000001",
+	title = "RM_SBTWEAKS-RVHS-SD01-META-000000000002",
+	subtitle = nil,
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000001", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000002", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000003", r = 1.00, g = 1.00, b = 1.00, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000004", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000005", r = 1.00, g = 1.00, b = 0.00, codes = "STS+0.2" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000006", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000007", r = 0.75, g = 0.00, b = 0.00, codes = "BOR-5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000008", r = 0.75, g = 0.00, b = 0.00, codes = "BOR-5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000009", r = 0.75, g = 0.00, b = 0.00, codes = "BOR-5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000010", r = 0.75, g = 0.00, b = 0.00, codes = "BOR-5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000011", r = 0.75, g = 0.00, b = 0.00, codes = "BOR-5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000012", r = 1.00, g = 1.00, b = 1.00, codes = "DOC+5" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000013", r = 0.70, g = 0.70, b = 0.70, codes = "BOR-1" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000014", r = 1.00, g = 1.00, b = 1.00, codes = "RCP=PurchaseSuperDaveShirt" },
+		{ text = "RM_SBTWEAKS_RVHS-SD01-LINE-000000000015", r = 0.50, g = 0.50, b = 0.50, codes = "BOR-2" },
 	},
 };
