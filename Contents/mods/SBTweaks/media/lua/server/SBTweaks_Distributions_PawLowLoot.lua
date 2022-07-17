7bc30b532f5bc0652bf6cffce034c1f4a24eb4f1
Orcicorn <orcicorn@gmail.com>
2022-07-17 19:00:27 -0400

    Readd some missing PLL spawns

---
+++ Contents/mods/SBTweaks/media/lua/server/SBTweaks_Distributions_PawLowLoot.lua
@@ -0,0 +1,122 @@
+require "Items/Distributions"
+require "Items/ProceduralDistributions"
+
+-- ADD BACK ITEMS TO PAW LOW LOOT THAT THE PAW LOW LOOT REBALANCE REMOVES
+-- WHY INSTEAD OF JUST REMOVING THE REBALANCE MOD?  THE REBALANCE MOD ALSO
+-- FIXES A LOT OF BROKEN SHIT BETWEEN PLL AND BRITA
+
+--MILITARY SPAWNS
+table.insert(ProceduralDistributions.list.ArmyHangarOutfit.items, "Gloves_Rogue")
+table.insert(ProceduralDistributions.list.ArmyHangarOutfit.items, 0.5)
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "Gloves_Rogue")
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.5)
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "RogueWaistBag_Military")
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.4)
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "Shoes_Rogue")
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.5)
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, "smallback")
+table.insert(ProceduralDistributions.list.ArmyStorageOutfit.items, 0.8)
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, "smallback")
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, 0.8)
+	
+--SURPLUS SPAWNS
+table.insert(ProceduralDistributions.list.ArmySurplusBackpacks.items, "smallback")
+table.insert(ProceduralDistributions.list.ArmySurplusBackpacks.items, 0.8)	
+table.insert(ProceduralDistributions.list.ArmySurplusFootwear.items, "Shoes_Rogue")
+table.insert(ProceduralDistributions.list.ArmySurplusFootwear.items, 0.5)	
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, "ClericNeck")
+table.insert(ProceduralDistributions.list.ArmySurplusHeadwear.items, 0.5)	
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "RogueWaistBag")
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 0.5)
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "BeltRig")
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 0.7)
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, "Gloves_Rogue")
+table.insert(ProceduralDistributions.list.ArmySurplusMisc.items, 0.5)	
+
+--CAMPING AND HUNTING SPAWNS
+table.insert(ProceduralDistributions.list.CampingStoreBackpacks.items, "BeltRig")
+table.insert(ProceduralDistributions.list.CampingStoreBackpacks.items, 0.7)
+table.insert(ProceduralDistributions.list.CampingStoreBackpacks.items, "smallback")
+table.insert(ProceduralDistributions.list.CampingStoreBackpacks.items, 1.5)
+table.insert(ProceduralDistributions.list.CrateCamping.items, "smallback")
+table.insert(ProceduralDistributions.list.CrateCamping.items, 0.4)
+table.insert(ProceduralDistributions.list.HuntingLockers.items, "smallback")
+table.insert(ProceduralDistributions.list.HuntingLockers.items, 0.4)
+
+--GUNSTORES
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.5)
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "RogueWaistBag")
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.5)
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Shoes_Rogue")
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.2)
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, "Gloves_Rogue")
+table.insert(ProceduralDistributions.list.GunStoreShelf.items, 0.3)
+
+--PAWN SPAWNS
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, "TKSword2")
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, 0.25)
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, "RogueSword2")
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, 0.35)
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, "ClericMace")
+table.insert(ProceduralDistributions.list.PawnShopKnives.items, 0.3)
+
+--POLICE PRISON AND SECURITY SPAWNS
+table.insert(ProceduralDistributions.list.PoliceLockers.items, "BeltRig")
+table.insert(ProceduralDistributions.list.PoliceLockers.items, 2)
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, "Hat_Altyn")
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, 0.6)
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, "HolsterPLL2")
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, 3)
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, "BeltRig")
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, 2)
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, "ClericNeck")
+table.insert(ProceduralDistributions.list.PoliceStorageOutfit.items, 0.5)
+table.insert(ProceduralDistributions.list.PrisonGuardLockers.items, "BeltRig")
+table.insert(ProceduralDistributions.list.PrisonGuardLockers.items, 2)
+table.insert(ProceduralDistributions.list.SecurityLockers.items, "BeltRig")
+table.insert(ProceduralDistributions.list.SecurityLockers.items, 0.2)
+table.insert(ProceduralDistributions.list.SecurityLockers.items, "TacticalWaistBagBack_Black")
+table.insert(ProceduralDistributions.list.SecurityLockers.items, 0.2)
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, "Animask1")
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, 1)
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, "Animask2")
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, 0.5)
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, "Animask12")
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, 1)
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, "Animask22")
+table.insert(ProceduralDistributions.list.SalonShelfHaircare.items, 0.5)
+
+--HOMES
+table.insert(ProceduralDistributions.list.BedroomSideTable.items, "smallback")
+table.insert(ProceduralDistributions.list.BedroomSideTable.items, 0.8)
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "TKSword2")
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.25)
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "RogueSword2")
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.35)
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "ClericMace")
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.3)
+
+--SPECIAL
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "TKSword2")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 2.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "RogueSword2")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "ClericMace")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.0)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "Gloves_Rogue")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 4.0)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "Shoes_Rogue")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 4.0)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "smallback")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 5.0)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "Vest_RogueVest")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "RogueWaist")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "RogueArms")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "RogueGreaves")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, "RogueHoodie")
+table.insert(ProceduralDistributions.list.MeleeWeapons.items, 3.5)
+
