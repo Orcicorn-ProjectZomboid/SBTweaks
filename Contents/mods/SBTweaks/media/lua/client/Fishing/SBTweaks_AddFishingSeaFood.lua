b5425f80db8cd2c8514e8ba64e088b88c9994881
Orcicorn <orcicorn@gmail.com>
2022-08-02 10:31:23 -0400

    Integrate more fishing options

--- Contents/mods/SBTweaks/media/lua/client/Fishing/SBTweaks_AddFishingSeaFood.lua
+++ Contents/mods/SBTweaks/media/lua/client/Fishing/SBTweaks_AddFishingSeaFood.lua
@@ -1,5 +1,4 @@
--- MOD: "Get Squid Crayfish and Shrim" (2710945663)
--- Adds Crayfish, Shrime and Squid to the fishing options
+-- Integration of multiple fishing mods to suit our needs
 
 --Crayfish
 local crayfish = {};
@@ -9,7 +8,11 @@ table.insert(crayfish.lure, "Worm");
 table.insert(crayfish.lure, "Cricket");
 table.insert(crayfish.lure, "Grasshopper");
 table.insert(crayfish.lure, "Cockroach");
+table.insert(crayfish.lure, "FishingTackle");
+table.insert(crayfish.lure, "FishingTackle2");
+table.insert(crayfish.lure, "BaitFish");
 table.insert(fishes, crayfish);
+
 --Shrimp
 local shrimp = {};
 shrimp.item = "Base.Shrimp";
@@ -19,6 +22,7 @@ table.insert(shrimp.lure, "Cricket");
 table.insert(shrimp.lure, "Grasshopper");
 table.insert(shrimp.lure, "Cockroach");
 table.insert(fishes, shrimp);
+
 --Squid
 local squid = {};
 squid.item = "Base.Squid";
@@ -27,4 +31,16 @@ table.insert(squid.lure, "Worm");
 table.insert(squid.lure, "Cricket");
 table.insert(squid.lure, "Grasshopper");
 table.insert(squid.lure, "Cockroach");
+table.insert(squid.lure, "FishingTackle");
+table.insert(squid.lure, "FishingTackle2");
+table.insert(squid.lure, "BaitFish");
 table.insert(fishes, squid);
+
+--Oysters
+local oysters = {};
+oysters.item = "Base.Oysters";
+oysters.lure = {};
+table.insert(oysters.lure, "Cricket");
+table.insert(oysters.lure, "Grasshopper");
+table.insert(oysters.lure, "Cockroach");
+table.insert(fishes, oysters);
